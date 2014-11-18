9735ac4789d66cbc48477c7f6456a45cf1dc1735
Mark Baker <mbaker@analecta.com>
2014-11-17 21:27:08 -0500

    Build intermediate tree
    Need to add text structures to tree

--- samparser.py
+++ samparser.py
@@ -4,29 +4,28 @@ import re
 import sys
 
 
-
 class SamParser:
     def __init__(self):
 
         self.stateMachine = StateMachine()
-        self.stateMachine.add_state("NEW", self.__new_file)
-        self.stateMachine.add_state("SAM", self.__sam)
-        self.stateMachine.add_state("BLOCK", self.__block)
-        self.stateMachine.add_state("CODEBLOCK-START", self.__codeblockStart)
-        self.stateMachine.add_state("CODEBLOCK", self.__codeblock)
-        self.stateMachine.add_state("PARAGRAPH-START", self.__paragraphStart)
-        self.stateMachine.add_state("PARAGRAPH", self.__paragraph)
-        self.stateMachine.add_state("RECORD-START", self.__recordStart)
-        self.stateMachine.add_state("RECORD", self.__record)
-        self.stateMachine.add_state("LIST-START", self.__listStart)
-        self.stateMachine.add_state("LIST", self.__listContinue)
-        self.stateMachine.add_state("NUM-LIST-START", self.__numListStart)
-        self.stateMachine.add_state("NUM-LIST", self.__numListContinue)
+        self.stateMachine.add_state("NEW", self._new_file)
+        self.stateMachine.add_state("SAM", self._sam)
+        self.stateMachine.add_state("BLOCK", self._block)
+        self.stateMachine.add_state("CODEBLOCK-START", self._codeblock_start)
+        self.stateMachine.add_state("CODEBLOCK", self._codeblock)
+        self.stateMachine.add_state("PARAGRAPH-START", self._paragraph_start)
+        self.stateMachine.add_state("PARAGRAPH", self._paragraph)
+        self.stateMachine.add_state("RECORD-START", self._record_start)
+        self.stateMachine.add_state("RECORD", self._record)
+        self.stateMachine.add_state("LIST-START", self._list_start)
+        self.stateMachine.add_state("LIST", self._list_continue)
+        self.stateMachine.add_state("NUM-LIST-START", self._num_list_start)
+        self.stateMachine.add_state("NUM-LIST", self._num_list_continue)
         self.stateMachine.add_state("END", None, end_state=1)
         self.stateMachine.set_start("NEW")
 
-        self.docStructure = DocStructure()
-
+        self.doc = DocStructure()
+        self.source = None
         self.patterns = {
             'comment': re.compile('\s*#.*'),
             'block-start': re.compile('(\s*)([a-zA-Z0-9-_]+):(.*)'),
@@ -39,171 +38,130 @@ class SamParser:
             'num-list-item': re.compile('(\s*)[0-9]+\.\s(.*)')
         }
 
-
     def parse(self, source):
         self.source = Source(source)
         self.stateMachine.run(self.source)
 
-    def __new_file(self, source):
-        line = source.nextLine
+    def _new_file(self, source):
+        line = source.next_line
         if line[:4] == 'sam:':
-            print('<?xml version="1.0" encoding="UTF-8"?>')
+
             return "SAM", source
         else:
             raise Exception("Not a SAM file!")
 
-    def __block(self, source):
+    def _block(self, source):
         line = source.currentLine
         match = self.patterns['block-start'].match(line)
-        localIndent = len(match.group(1))
-        localElement = Element(match.group(2))
-        localContent = match.group(3).strip()
+        local_indent = len(match.group(1))
+        local_element = match.group(2).strip()
+        local_content = match.group(3).strip()
 
-        if self.docStructure.pendingContent:
-            print(self.docStructure.getPendingContent(), end="")
-
-        while localIndent <= self.docStructure.currentIndent:
-            print(self.docStructure.popElement)
-
-        if localContent[:1] == ':':
-            self.docStructure.pushElement(localElement, localIndent)
+        if local_content[:1] == ':':
             return "RECORD-START", source
         else:
-            print(self.docStructure.pushElement(localElement, localIndent), end="")
-            if localContent.strip() != "":
-                self.docStructure.setPendingContent(localContent)
+            self.doc.new_block(local_element, local_content, local_indent)
             return "SAM", source
 
-
-    def __codeblockStart(self, source):
+    def _codeblock_start(self, source):
         line = source.currentLine
-        localIndent = len(line) - len(line.lstrip(' '))
-        while localIndent <= self.docStructure.currentIndent:
-            print(self.docStructure.popElement)
+        local_indent = len(line) - len(line.lstrip(' '))
         match = self.patterns['codeblock-start'].match(line)
         language = match.group(2).strip()
-        print(self.docStructure.pushElement(Element('codeblock', {'language': language}), 0) + '<![CDATA[')
+        self.doc.new_block('codeblock', '', local_indent)
         return "CODEBLOCK", source
 
-
-    def __codeblock(self, source):
-        line = source.nextLine
+    def _codeblock(self, source):
+        line = source.next_line
         if self.patterns['codeblock-end'].match(line):
-            print("]]>" + self.docStructure.popElement)
             return "SAM", source
         else:
-            print(line, end='')
             return "CODEBLOCK", source
 
-
-    def __paragraphStart(self, source):
+    def _paragraph_start(self, source):
         line = source.currentLine
-        localIndent = len(line) - len(line.lstrip(' '))
-        while localIndent <= self.docStructure.currentIndent:
-            print(self.docStructure.popElement)
-        print(self.docStructure.pushElement(Element('p'), 0), end="")
-        self.docStructure.paragraphStart(line)
+        local_indent = len(line) - len(line.lstrip(' '))
+        self.doc.new_block('p', '', local_indent)
+        self.doc.paragraph_start(line)
         return "PARAGRAPH", source
 
-
-    def __paragraph(self, source):
-        line = source.nextLine
+    def _paragraph(self, source):
+        line = source.next_line
         if self.patterns['blank-line'].match(line):
 
-            paraParser = SamParaParser(self.docStructure.currentParagraph)
-            paraParser.parse()
-            #self.__processParagraph(self.docStructure.currentParagraph)
+            para_parser = SamParaParser(self.doc.currentParagraph)
+            para_parser.parse()
+            #self.__process_paragraph(self.doc.currentParagraph)
 
-            print(self.docStructure.popElement)
             return "SAM", source
         else:
-            self.docStructure.paragraphAppend(line)
+            self.doc.paragraph_append(line)
             return "PARAGRAPH", source
 
-    def __listStart(self, source):
+    def _list_start(self, source):
         line = source.currentLine
-        localIndent = len(line) - len(line.lstrip(' '))
-        while localIndent <= self.docStructure.currentIndent:
-            print(self.docStructure.popElement)
-        print(self.docStructure.pushElement(Element('ul'), 0))
+        local_indent = len(line) - len(line.lstrip(' '))
+        self.doc.new_block('ul', '', local_indent)
         match = self.patterns['list-item'].match(line)
-        print('<li>' + match.group(2).strip() + '</li>')
+        self.doc.new_block('li', 'str(match.group(2)).strip()', local_indent)
         return "LIST", source
 
-
-    def __listContinue(self, source):
-        line = source.nextLine
+    def _list_continue(self, source):
+        line = source.next_line
+        local_indent = len(line) - len(line.lstrip(' '))
         if self.patterns['blank-line'].match(line):
-            print(self.docStructure.popElement)
             return "SAM", source
         elif self.patterns['list-item'].match(line):
             match = self.patterns['list-item'].match(line)
-            print('<li>' + match.group(2).strip() + '</li>')
+            self.doc.new_block('li', 'str(match.group(2)).strip()', local_indent)
             return "LIST", source
         else:
             raise Exception("Broken list at line " + str(source.currentLineNumber) + " " + source.filename)
 
-    def __numListStart(self, source):
+    def _num_list_start(self, source):
         line = source.currentLine
-        localIndent = len(line) - len(line.lstrip(' '))
-        while localIndent <= self.docStructure.currentIndent:
-            print(self.docStructure.popElement)
-        print(self.docStructure.pushElement(Element('ol'), 0))
+        local_indent = len(line) - len(line.lstrip(' '))
+        self.doc.new_block('ul', '', local_indent)
         match = self.patterns['num-list-item'].match(line)
-        print('<li>' + match.group(2).strip() + '</li>')
+        self.doc.new_block('li', 'str(match.group(2)).strip()', local_indent)
         return "NUM-LIST", source
 
-
-    def __numListContinue(self, source):
-        line = source.nextLine
+    def _num_list_continue(self, source):
+        line = source.next_line
+        local_indent = len(line) - len(line.lstrip(' '))
         if self.patterns['blank-line'].match(line):
-            print(self.docStructure.popElement)
             return "SAM", source
         elif self.patterns['num-list-item'].match(line):
             match = self.patterns['num-list-item'].match(line)
-            print('<li>' + match.group(2).strip() + '</li>')
+            self.doc.new_block('li', 'str(match.group(2)).strip()', local_indent)
             return "NUM-LIST", source
         else:
             raise Exception("Broken num list at line " + str(source.currentLineNumber) + " " + source.filename)
 
-
-    def __recordStart(self, source):
+    def _record_start(self, source):
         line = source.currentLine
-        fieldNames = [x.strip() for x in self.patterns['record-start'].match(line).group(1).split(',')]
-        self.docStructure.fields = fieldNames
+        match = self.patterns['block-start'].match(line)
+        local_indent = len(match.group(1))
+        local_element = match.group(2).strip()
+        field_names = [x.strip() for x in self.patterns['record-start'].match(line).group(1).split(',')]
+        self.doc.new_record_set(local_element, field_names, local_indent)
         return "RECORD", source
 
-
-    def __record(self, source):
-        line = source.nextLine
+    def _record(self, source):
+        line = source.next_line
         if self.patterns['blank-line'].match(line):
-            self.docStructure.popElement
             return "SAM", source
         else:
-            print("<" + self.docStructure.currentElement.name + ">")
-            fieldValues = [x.strip() for x in line.split(',')]
-            record = zip(self.docStructure.fields, fieldValues)
-            for key, value in record:
-                print('<' + key + '>' + value + '</' + key + '>')
-            print("</" + self.docStructure.currentElement.name + ">")
+            field_values = [x.strip() for x in line.split(',')]
+            record = list(zip(self.doc.fields, field_values))
+            self.doc.new_record(record)
             return "RECORD", source
 
-
-    def __sam(self, source):
-        line = source.nextLine
+    def _sam(self, source):
+        line = source.next_line
         if line == "":
-            if self.docStructure.pendingContent is not None:
-                print(self.docStructure.getPendingContent(), end='')
-
-            while True:
-                try:
-                    print(self.docStructure.popElement)
-                except IndexError:
-                    break
             return "END", source
         elif self.patterns['comment'].match(line):
-            if self.docStructure.pendingContent is not None:
-                print(self.docStructure.getPendingContent(), end='')
             print('<!--' + line.strip()[1:] + '-->')
             return "SAM", source
         elif self.patterns['block-start'].match(line):
@@ -211,8 +169,6 @@ class SamParser:
         elif self.patterns['blank-line'].match(line):
             print()
             return "SAM", source
-        elif self.docStructure.pendingContent:
-            raise Exception("Unmatched content found: " + self.docStructure.pendingContent )
         elif self.patterns['codeblock-start'].match(line):
             return "CODEBLOCK-START", source
         elif self.patterns['list-item'].match(line):
@@ -227,7 +183,7 @@ class SamParser:
         else:
             raise Exception("I'm confused")
 
-    def __processParagraph(self, paragraph):
+    def _process_paragraph(self, paragraph):
 
         parts = self.patterns['annotation'].split(paragraph)
         for part in parts:
@@ -235,65 +191,104 @@ class SamParser:
             if p is None:
                 print(part, end="")
             else:
-                print('<annotation type="' + p.group(2).strip() + '">' + p.group(1) + '</annotation>', end="")
+                print('<annotation type="' + str(p.group(2)).strip() + '">' + p.group(1) + '</annotation>', end="")
+
 
 class Element:
-    def __init__(self, name, attributes={}):
+    def __init__(self, name, attributes=None):
         self.name = name
         assert isinstance(attributes, dict)
         self.attributes = attributes
 
+class Block:
+    def __init__(self, name, content, indent):
+        self.name = name
+        self.content = content
+        self.indent = indent
+        self.parent = None
+        self.children = []
+
+    def add_child(self, b):
+        b.parent = self
+        self.children.append(b)
+
+    def add_sibling(self, b):
+        b.parent = self.parent
+        self.parent.children.append(b)
+
+    def add_at_indent(self, b, indent):
+        x = self.parent
+        while x.indent > indent:
+            x = x.parent
+        b.parent = x.parent
+        x.children.append(b)
+
+    def __str__(self):
+        return ''.join(self._output_block())
+
+    def _output_block(self):
+        yield " " * self.indent
+        yield "[%s:'%s'" % (self.name, self.content)
+        for x in self.children:
+            yield "\n"
+            yield ''.join(x._output_block())
+        yield "]"
+
+
 
 class DocStructure:
     def __init__(self):
-        self.elements = []
-        self.fields = []
-        self.currentParagraph = ""
-        self.pendingContent = None
+        self.doc = None
+        self.fields = None
+        self.current_record = None
+        self.current_paragraph = None
+        self.current_block = None
 
-    def pushElement(self, element, currentIndent):
-        self.elements.append({'element': element, 'indent': currentIndent})
-        tag = "<" + element.name
-        for key, value in element.attributes.items():
-            tag += " " + key + '="' + value + '"'
-        tag += '>'
-        return tag
+    def new_block(self, block_type, text, indent):
+        b = Block(block_type, text, indent)
+        if self.doc is None:
+            self.doc = b
+        elif self.current_block.indent < indent:
+            self.current_block.add_child(b)
+        elif self.current_block.indent == indent:
+            self.current_block.add_sibling(b)
+        else:
+            self.current_block.add_at_indent(b, indent)
+        self.current_block = b
+        print(self.doc)
+        print('-----------------------------------------------------')
+
+    def new_record_set(self, local_element, field_names, local_indent):
+        self.current_record = {'local_element': local_element, 'local_indent': local_indent}
+        self.fields = field_names
+
+    def new_record(self, record):
+        b = Block(self.current_record['local_element'], '', self.current_record['local_indent'])
+        self.current_block.add_child(b)
+        self.current_block = b
+        for name, content in record:
+            b = Block(name, content, self.current_block.indent+4)
+            self.current_block.add_child(b)
+
 
     @property
-    def popElement(self):
-        tag = "</" + self.elements[-1]['element'].name + ">"
-        self.elements.pop()
-        return tag
-
-    @property
-    def currentElement(self):
+    def current_element(self):
         if not self.elements:
             return None
         else:
             return self.elements[-1]['element']
 
     @property
-    def currentIndent(self):
-        if not self.elements:
+    def current_indent(self):
+        if not self.stack[-1]['indent']:
             return -1
         else:
-            return self.elements[-1]["indent"]
+            return self.stack[-1]["indent"]
 
-
-    def getPendingContent(self):
-        p = self.pendingContent
-        self.pendingContent = None
-        return p
-
-    def setPendingContent(self, content):
-        assert self.pendingContent is None
-        self.pendingContent = content
-
-
-    def paragraphStart(self, line):
+    def paragraph_start(self, line):
         self.currentParagraph = line.strip()
 
-    def paragraphAppend(self, line):
+    def paragraph_append(self, line):
         self.currentParagraph += " " + line.strip()
 
 
@@ -309,7 +304,7 @@ class Source:
         self.currentLineNumber = 0
 
     @property
-    def nextLine(self):
+    def next_line(self):
         self.currentLine = self.sourceFile.readline()
         self.currentLineNumber += 1
         return self.currentLine
@@ -319,7 +314,3 @@ if __name__ == "__main__":
     samParser = SamParser()
     filename = sys.argv[-1]
     samParser.parse(filename)
-
-
-
-
